5ffd0cd463fe7be1c3f0a9ad95613cd861f13236
Marc Joos <marc.joos@gmail.com>
2014-11-26 11:48:06 +0100

    Add a FileTree class to manage the directory tree and the source files within

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
-import re
+import os, re, shutil
 
 class File:
     def __init__(self, frname, precision=r"d0", verbose=False):
@@ -36,3 +36,77 @@ class File:
         f = open(self.fwname, 'w')
         f.writelines(self.newlines)
         f.close()
+
+class FileTree:
+    def __init__(self, inputDir="./", tmp=True):
+        self.dir = inputDir
+        self.tmp = tmp
+
+    def listFiles(self):
+        self.listDirs = []
+        self.listFiles = []
+        self._listIgnore = []
+        for root, dirs, files in os.walk(self.dir):
+            self._listFiles(root, files)
+        self._cleanFiles()
+
+    def _listFiles(self, root, files):
+        if root.split('/')[-1][0] != '.' or len(root.split('/')[-1]) == 1:
+            if self._listIgnore:
+                for ignore in self._listIgnore:
+                    if root[:len(ignore)] == ignore:
+                        return
+                    else:
+                        if files:
+                            self.listDirs.append(root)
+                            self.listFiles.append(files)
+            else:
+                if files:
+                    self.listDirs.append(root)
+                    self.listFiles.append(files)
+        else:
+            self._listIgnore.append(root)
+
+    def _cleanFiles(self):
+        if self.listDirs:
+            for i, listFiles in enumerate(self.listFiles):
+                iterList = listFiles.__iter__()
+                filesToRemove = []
+                for file_ in iterList:
+                    if not(file_[-4:] in ['.f90', '.F90'] or \
+                       file_[-2:] in ['.f', '.F']):
+                        filesToRemove.append(file_)
+                if filesToRemove:
+                    for file_ in filesToRemove:
+                        self.listFiles[i].remove(file_)
+            for i, listFiles in enumerate(self.listFiles):
+                if not listFiles: 
+                    self.listFiles.remove(self.listFiles[i])
+                    self.listDirs.remove(self.listDirs[i])
+
+    def _removeTrailingDot(self, listDirs):
+        cleanDirs = []
+        trailingDot = re.compile('\.\.\/')
+        for dir_ in listDirs:
+            cleanDirs.append(trailingDot.sub('', dir_))
+        return cleanDirs
+
+    def createTmpTree(self, tmpRoot='./'):
+        self.tmpTree = self._removeTrailingDot(self.listDirs)
+        workDir = os.getcwd()
+        npardir = tmpRoot.count('../')
+        if npardir > 0:
+            workDirRoot = "/".join(workDir.split('/')[:-npardir]) + '/'
+            print self._removeTrailingDot([tmpRoot])
+            workDirRoot += self._removeTrailingDot([tmpRoot])[0]
+        else:
+            workDirRoot = workDir + '/' + tmpRoot
+        for dir_ in self.tmpTree:
+            try:
+                os.makedirs(workDirRoot + dir_)
+            except OSError:
+                pass
+        for i, dir_ in enumerate(self.tmpTree):
+            for file_ in self.listFiles[i]:
+                shutil.copy(self.listDirs[i] + '/' + file_, dir_ + '/' + file_)
+        
